4dd7c407f1d00bac14fe9f452156a7f92cc8a40a
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2020-12-04 20:57:15 -0500

    Adjust to new event consumer pattern.

--- aos-base/src/main/java/com/atlas/aos/Server.java
+++ aos-base/src/main/java/com/atlas/aos/Server.java
@@ -1,12 +1,9 @@
 package com.atlas.aos;
 
 import java.net.URI;
-import java.util.concurrent.Executors;
 
 import com.atlas.aos.event.consumer.CharacterStatusConsumer;
-import com.atlas.csrv.constant.EventConstants;
-import com.atlas.csrv.event.CharacterStatusEvent;
-import com.atlas.kafka.consumer.ConsumerBuilder;
+import com.atlas.kafka.consumer.SimpleEventConsumerFactory;
 import com.atlas.shared.rest.RestServerFactory;
 import com.atlas.shared.rest.RestService;
 import com.atlas.shared.rest.UriBuilder;
@@ -17,15 +14,9 @@ public class Server {
    public static void main(String[] args) {
       PersistenceManager.construct("atlas-aos");
 
+      SimpleEventConsumerFactory.create(new CharacterStatusConsumer());
+
       URI uri = UriBuilder.host(RestService.ACCOUNT).uri();
       RestServerFactory.create(uri, "com.atlas.aos.rest");
-
-      Executors.newSingleThreadExecutor().execute(
-            new ConsumerBuilder<>("Account Service", CharacterStatusEvent.class)
-                  .setBootstrapServers(System.getenv("BOOTSTRAP_SERVERS"))
-                  .setTopic(System.getenv(EventConstants.TOPIC_CHARACTER_STATUS))
-                  .setHandler(new CharacterStatusConsumer())
-                  .build()
-      );
    }
 }
--- aos-base/src/main/java/com/atlas/aos/event/consumer/CharacterStatusConsumer.java
+++ aos-base/src/main/java/com/atlas/aos/event/consumer/CharacterStatusConsumer.java
@@ -2,15 +2,36 @@ package com.atlas.aos.event.consumer;
 
 import com.atlas.aos.LoginState;
 import com.atlas.aos.processor.AccountProcessor;
+import com.atlas.csrv.constant.EventConstants;
 import com.atlas.csrv.event.CharacterStatusEvent;
-import com.atlas.kafka.consumer.ConsumerRecordHandler;
+import com.atlas.kafka.consumer.SimpleEventHandler;
 
-public class CharacterStatusConsumer implements ConsumerRecordHandler<Long, CharacterStatusEvent> {
+public class CharacterStatusConsumer implements SimpleEventHandler<CharacterStatusEvent> {
    @Override
    public void handle(Long aLong, CharacterStatusEvent event) {
       switch (event.type()) {
          case LOGIN -> AccountProcessor.getInstance().updateLoggedInStatus(event.accountId(), LoginState.LOGGED_IN);
          case LOGOUT -> AccountProcessor.getInstance().updateLoggedInStatus(event.accountId(), LoginState.NOT_LOGGED_IN);
       }
+   }
+
+   @Override
+   public Class<CharacterStatusEvent> getEventClass() {
+      return CharacterStatusEvent.class;
+   }
+
+   @Override
+   public String getConsumerId() {
+      return "Account Service";
+   }
+
+   @Override
+   public String getBootstrapServers() {
+      return System.getenv("BOOTSTRAP_SERVERS");
+   }
+
+   @Override
+   public String getTopic() {
+      return System.getenv(EventConstants.TOPIC_CHARACTER_STATUS);
    }
 }
